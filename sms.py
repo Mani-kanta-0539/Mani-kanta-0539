ce8bde14334e50257c1158623a4d5b37679a7383
Mani_kanta_359 <112759657+Mani-kanta-0539@users.noreply.github.com>
2022-12-28 11:27:54 +0530

    Delete sms.py

--- sms.py
+++
@@ -1,8 +0,0 @@
-from twilio.rest import Client
-
-account = "ACXXXXXXXXXXXXXXXXX"
-token = "YYYYYYYYYYYYYYYYYY"
-client = Client(account, token)
-
-message = client.messages.create(to="91+7995726714", from_="+15555555555",
-                                 body="Hello there!")
